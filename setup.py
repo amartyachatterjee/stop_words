3c3df0a2079018da05a53864a88db198f1f85624
amartyachatterjee <amartya.chatterjee@gmail.com>
2025-12-09 17:36:24 -0600

    Update setup.py
    Signed-off-by: amartyachatterjee <amartya.chatterjee@gmail.com>

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     name='stop_words',
     version='0.1.0',
     description='A collection of data structures to remove stop words, including domain-specific stopwords.',
-    # long_description=open('README.md').read(),
+    long_description=open('README.md').read(),
     author='Amartya Chatterjee',
     author_email='amartya.chatterjee@gmail.com',
     license='MIT',
